64d3a7174ee5d3a7cb82139a29c1a6b04d319ce8
irvancolz <irvansaharudin@gmail.com>
2025-08-23 10:16:37 +0700

    add coming soon label in topnav url

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import App from "./App";
-import ImageViewer from "./Comps/ImageViewer/ImageViewer";
 import "./index.scss";
+
+const $comingSoonLinks = document.querySelectorAll(".link.coming_soon");
+$comingSoonLinks.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    el.classList.add("clicked");
+  });
+});
 
 const app = new App({ content: document.getElementById("app") });
 window.addEventListener("load", async () => {
