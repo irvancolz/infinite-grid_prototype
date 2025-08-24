8f51a8cea37deb3ce81b017162675c8efd8c46a5
irvancolz <irvansaharudin@gmail.com>
2025-08-24 16:34:50 +0700

    refactor sizes class

--- src/Utils/Sizes.js
+++ src/Utils/Sizes.js
@@ -1,24 +1,62 @@
+import { CAMERA_DISTANCE } from "../const";
+import { getWorldWidthFromPx } from "../math";
 import EventEmitter from "./EventEmitter";
+import * as THREE from "three";
 
+let instance;
 export default class Sizes extends EventEmitter {
   constructor() {
     super();
+    if (instance) return instance;
 
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
-    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+    instance = this;
     this.fov = 75;
+
+    this._calculate();
 
     window.addEventListener("resize", () => {
       this.resize();
     });
   }
 
-  resize() {
+  _calculate() {
     this.width = window.innerWidth;
     this.height = window.innerHeight;
+    this.aspect = this.width / this.height;
     this.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
+    this.frustumHeight =
+      2 * CAMERA_DISTANCE * Math.tan(THREE.MathUtils.degToRad(this.fov / 2));
+    this.frustumWidth = this.frustumHeight * this.aspect;
+
+    this.PLANE_RATIO = 853 / 1280;
+    const width = Math.max(this.frustumWidth / 4, getWorldWidthFromPx(160));
+    const height = (width * 1280) / 853;
+    this.PLANE = {
+      width,
+      height,
+    };
+  }
+
+  resize() {
+    this._calculate();
+
     this.trigger("resize");
+  }
+
+  getHeightInPx(worldHeight = 0) {
+    return (this.height / this.frustumHeight) * worldHeight;
+  }
+
+  getWidthInPx(worldWidth = 0) {
+    return (this.width / this.frustumWidth) * worldWidth;
+  }
+
+  getWorldHeightFromPx(px = 0) {
+    return (this.frustumHeight / this.height) * px;
+  }
+
+  getWorldWidthFromPx(px = 0) {
+    return (this.frustumWidth / this.width) * px;
   }
 }
