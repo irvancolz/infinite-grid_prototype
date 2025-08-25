bcef18940f6757053e9c84e621f6259794b72365
irvancolz <irvansaharudin@gmail.com>
2025-08-25 09:55:06 +0700

    resolve body overflow issue

--- src/main.js
+++ src/main.js
@@ -6,10 +6,19 @@ const topnav = new Topnav();
 
 const app = new App({ content: document.getElementById("app") });
 window.addEventListener("load", async () => {
+  const name = location.hash.split("");
+  name.splice(0, 2);
+  const joined = name.join("");
+  document.body.className = "body-" + joined;
+
   await app.render();
 });
 
 window.addEventListener("hashchange", async () => {
+  const name = location.hash.split("");
+  name.splice(0, 2);
+  const joined = name.join("");
+  document.body.className = "body-" + joined;
   app.dispose();
   await app.render();
 });
